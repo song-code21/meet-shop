b4a8f7333f074a5dd1d2834f985ee65034c8591c
song-code21 <wkcjf131313@gmail.com>
2026-01-07 11:31:58 +0900

    feat: event페이지 구축

---
+++ app/events/page.tsx
@@ -0,0 +1,12 @@
+const EventsPage = () => {
+  return (
+    <div>
+      <div className="p-6">
+        <h1 className="text-center text-xl font-bold">이벤트/기획전</h1>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default EventsPage;
