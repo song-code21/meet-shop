f727b0bf2da3266e04afc810c77fa75d2c6a457d
song-code21 <wkcjf131313@gmail.com>
2026-01-12 13:48:01 +0900

    feat: admin 레이아웃 및 메인페이지 구축

---
+++ app/(admin)/admin/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import Header from "@/app/(components)/Header";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ko">
+      <body className="bg-zinc-50 min-h-screen">
+        <Header />
+        <main className="mx-auto min-h-screen max-w-5xl px-4 py-6">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
